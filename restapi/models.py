05bf1e3378be69b121969b713c55ea33d5bff479
varunpandey2106 <varun.pandey2106@gmail.com>
2023-07-09 19:12:44 +0530

    models, tests updated. migrations made

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from .tests import testModels
 
 # Create your models here.
+
+
+class Expense(models.Model):
+    amount_in_INR=models.CharField(max_length=200)
+    merchant= models.CharField(max_length=200)
+    brand= models.CharField(max_length=200)
+    description= models.TextField(max_length=200, blank=True, null= True)
+    category= models.CharField(max_length=200)
+    date_created= models.DateTimeField(auto_now_add=True)
+    date_updated= models.DateTimeField(auto_now=True)
+    
+
+
+
+
+
+    
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
+
+class testModels(TestCase):
+    def test_expense(self):
+        expense= models.expense.objects.create(amount_in_INR=1500, merchant="amazon", brand="boat", description="earpods", category="gadgets")
+        inserted_expense=models.Exepense.objects.get(pk=expense.id)
+
+        self.assertEqual(1500, inserted_expense.amount_in_INR)
+        self.assertEqual(1500, inserted_expense.merchant)
+        self.assertEqual(1500, inserted_expense.brand)
+        self.assertEqual(1500, inserted_expense.description)
+        self.assertEqual(1500, inserted_expense.category)
+
+
+
+
